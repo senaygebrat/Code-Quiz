d7aa5d19fa9b40d8a399329a94b7d6a7a3591a34
Senay Gebrat <senaygebrat@gmail.com>
2022-10-03 21:02:49 -0700

    added functions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Code-Quiz
+
+# About Code Quiz
+
+This is a timed code quiz that utilizes HTML, CSS, and JavaScript. There are a few questions for the user, and when incorrectly answered time will be taken off the clock.
+
+# Code Snippet
+
+# Author Links
+
+[LinkedIn](https://www.linkedin.com/in/senay-gebrat-566b78250/)
+[Github](https://github.com/senaygebrat)
--- script.js
+++ script.js
@@ -1,23 +1,75 @@
 //need a generate.btn add.eventListener("click, start game~~push to start") to start the quiz
 document.getElementById("question-div").style.display = "none";
 document.getElementById("initials-div").style.display = "none";
-var timerId;
-var timeLeft = 10;
+// var timerId;
+var timeLeft = 3;
+// var choices = Array.from(document.getElementsByClassName("answer"));
+var questionEl = document.getElementById("question");
+var answerEl = document.getElementById("answer-container");
+var timeEl = document.getElementById("time"); //do i need this here?
 
-//create an array of questions with question, an array of options and right answer values
+//create an array of questions with question, an array of objects and right answer values
+
+var questions = [
+    {
+        question: "Strings must be surrounded by what?",
+        options: ["commas", "quotations", "parentheses"],
+        answer: "parentheses"
+    },
+    {
+        question: "What command in terminal will change your current directory?",
+        options: ["cd", "pwd", "ls"],
+        answer: "cd"
+    },
+    {
+        question: "Which website do we use for updating repositories?",
+        options: ["Slack", "Bootcampspot", "Github"],
+        answer: "Github"
+    },
+    {
+        question: "CSS is commonly used for what purpose?",
+        options: ["styling", "structuring", "scripting"],
+        answer: "styling"
+    },
+    {
+        question: "When would you need a DS_Store?",
+        choice1: ["index.html", ".gitignore", "style.css"],
+        answer: ".gitignore"
+    },
+    {
+        question: "What is a valid boolean response?",
+        choice1: ["string", "true", "50"],
+        answer: "true"
+    }
+]
+
+var questionIndex = 0;
+var timerId = 0;
 
 
 document.getElementById("start").addEventListener("click", function(){
     document.getElementById("start-div").style.display = "none";
     document.getElementById("question-div").style.display = "block";
     document.getElementById("initials-div").style.display = "block";
+    
 
-    timerId = setInterval(function(){
+    var showQuestion = question[questionIndex].question;
+    function nextQuestion(){
+        
+    }
+
+
+   var timerId = setInterval(function(){
+    document.getElementById("time").textContent = timeLeft;
+
         if(timeLeft === 0)
         {
-            clearInterval(timeInterval)
-            displayMessage()
+            clearInterval(timerId)
+            alert("Time's up!")
+            // displayMessage()
             //clear timer
+            document.getElementById("initials-div").style.display = "block";
+
             //hide questions div and show initials div
         }
         else{
@@ -26,3 +78,62 @@ document.getElementById("start").addEventListener("click", function(){
         }
     }, 1000)
 })
+    clearInterval();
+    // }
+    
+
+    // })
+
+// document.getElementById("start").addEventListener("click", function(){
+//     document.getElementById("start-div").style.display = "none";
+//     document.getElementById("question-div").style.display = "block";
+//     document.getElementById("initials-div").style.display = "block";
+
+//     timerId = setInterval(function(){
+//         if(timeLeft === 0)
+//         {
+//             clearInterval(timeInterval)
+//             displayMessage()
+//             //clear timer
+//             //hide questions div and show initials div
+//         }
+//         else{
+//             document.getElementById("time").textContent = timeLeft;
+//             timeLeft--;
+//         }
+//     }, 1000)
+// })
+
+var score = 0;
+
+function questionArray(){
+    var firstQuestion = questions[questionArray];
+    questionEl.textContent = firstQuestion.question;
+    var options = firstQuestion.options;
+    answerEl.textContent = options;
+}
+questionArray();
+
+
+
+
+
+var wrongAnswer = 10;
+// function getNewQuestion(){
+//     if
+
+// }
+
+// function showQuestion(q){
+//     questionDiv = document.getElementById("Question");
+//     questionDiv.textContent = q.questionDiv
+
+//     var alternative = document.querySelectorAll("#alternative");
+// }
+
+// showQuestion(questions);
+// function nextQuestion(){
+
+// }
+
+// function chooseAnswer(){}
